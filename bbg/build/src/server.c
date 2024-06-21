53cfdc2713427ebcdf5ced469130db306dc33de8
morris <morris.elkanaev@gmail.com>
2024-06-21 17:15:21 +0300

    Modified in bbg/build/src/ [Makefile, server.c, tcp_client.c]

--- bbg/build/src/server.c
+++ bbg/build/src/server.c
@@ -1,4 +1,12 @@
-/* gcc server.c -o out_server */
+/* server.c - Версия 1.1
+ * Изменения:
+ * - Добавлена поддержка многопоточности
+ * - Добавлены мьютексы и семафоры для синхронизации
+ * - Обновлено управление параллелизмом
+ * - Добавлена обработка сигналов для корректного завершения работы
+ */
+
+/* gcc server.c -o out_server -lpthread */
 /* server.c */
 
 #include <stdio.h>
@@ -6,89 +14,203 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <pthread.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <semaphore.h>
+#include <signal.h>
 
 #define SERVER_PORT 12345
 #define BUFFER_SIZE 1024
+#define SHM_KEY 0x1234
+#define MAX_CLIENTS 10
+#define VERSION "1.1"
 
-int main()
-{
-	int                  ssck;
-	int                  csck;
-	struct  sockaddr_in  saddr;
-	struct  sockaddr_in  caddr;
-	char                 buffer[BUFFER_SIZE + 1];   /* +1 for adding '\0' */
-	socklen_t            caddrlen = sizeof(caddr);
+// Shared memory structure
+struct shared_data {
+    char data[BUFFER_SIZE];
+};
 
-	int                  tbrecv;	     /* Total bytes received from client */
-	int                  brecv;
-	char                 *line;   /* Split the buffer into lines */
+// Thread argument structure
+struct thread_arg {
+    int csck;
+    struct sockaddr_in caddr;
+};
 
+// Mutex for shared memory synchronization
+pthread_mutex_t shm_mutex = PTHREAD_MUTEX_INITIALIZER;
 
-	/* Create a socket */
-	if ((ssck = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-	{
-		perror("socket");
-		exit(EXIT_FAILURE);
-	}
+// Semaphore for controlling the number of concurrent clients
+sem_t client_sem;
 
-	saddr.sin_family = AF_INET;
-	saddr.sin_port = htons(SERVER_PORT);
-	saddr.sin_addr.s_addr = INADDR_ANY;
+// Flag to control the main loop
+volatile sig_atomic_t running = 1;
 
-	/* Bind the socket to the port */
-	if (bind(ssck, (struct sockaddr *)&saddr, sizeof(saddr)) < 0)
-	{
-		perror("bind");
-		exit(EXIT_FAILURE);
-	}
+// Signal handler for graceful shutdown
+void signal_handler(int signum) {
+    running = 0;  // Set flag to stop the main loop
+    printf("Received signal %d, shutting down...\n", signum);
+}
 
-	/* Listen for incoming connections */
-	if (listen(ssck, 5) < 0)
-	{
-		perror("listen");
-		exit(EXIT_FAILURE);
-	}
+void *handle_client(void *arg) {
+    struct thread_arg *targ = (struct thread_arg *)arg;
+    int csck = targ->csck;
+    struct sockaddr_in caddr = targ->caddr;
+    char buffer[BUFFER_SIZE + 1];
 
-	printf("Server is listening on port %d\n", SERVER_PORT);
+	int                 tbrecv;         /* Total bytes received from client */
+    int 				brecv;
+    char 				*line;			// Split the buffer into lines
 
-	while (1)
-	{
+    // Attach shared memory
+    int shm_id = shmget(SHM_KEY, sizeof(struct shared_data), 0666);
+    if (shm_id == -1) {
+        perror("shmget");
+        close(csck);
+        free(arg);
+        pthread_exit(NULL);
+    }
+    struct shared_data *shm_data = (struct shared_data *)shmat(shm_id, NULL, 0);
+    if (shm_data == (void *)-1) {
+        perror("shmat");
+        close(csck);
+        free(arg);
+        pthread_exit(NULL);
+    }
+
+    // Read data from the client and print it to the terminal
+    while ((brecv = recv(csck, buffer, BUFFER_SIZE, 0)) > 0) {
+
 		tbrecv = 0;
 
-		/* Accept a connection from a client */
-		csck = accept(ssck, (struct sockaddr *)&caddr, &caddrlen);
-		if (csck < 0)
-		{
-			perror("accept");
-			exit(EXIT_FAILURE);
-		}
+        buffer[brecv] = '\0';       /* Null-terminate the string */
+        line = strtok(buffer, "\n");       /* Tokenize within the recv loop */
+        while (line != NULL) {
+            printf("Received from %s: %s\n", inet_ntoa(caddr.sin_addr), line);  /* Print each line */
 
-		/* Read data from the client and print it to the terminal */
-		while ((brecv = recv(csck, buffer, BUFFER_SIZE, 0)) > 0)
-		{
-			buffer[brecv] = '\0';       /* Null-terminate the string */
-			line = strtok(buffer, "\n");       /* Tokenize within the recv loop */
-			while (line != NULL)
-			{
-				printf("Received: %s\n", line);  /* Print each line */
-				line = strtok(NULL, "\n");       /* Move to the next line */
-			}
-			tbrecv += brecv;
-		}
+            // Lock mutex
+            pthread_mutex_lock(&shm_mutex);
 
-		/* Print a message indicating the end of data reception from the client */
-		if (tbrecv > 0)
-		{
-			printf("Complete message received from client.\n");
-		}
+            // Write to shared memory
+            strncpy(shm_data->data, line, BUFFER_SIZE);
 
-		/* Close the client socket */
-		close(csck);
+            // Unlock mutex
+            pthread_mutex_unlock(&shm_mutex);
+
+            line = strtok(NULL, "\n");       /* Move to the next line */
+        }
+
+		tbrecv += brecv;
+    }
+
+	/* Print a message indicating the end of data reception from the client */
+	if (tbrecv > 0)
+	{
+		printf("Complete message received from client.\n");
 	}
 
-	/* Close the server socket */
-	close(ssck);
+    // Detach shared memory
+    shmdt(shm_data);
 
-	return 0;
+    // Close the client socket
+    close(csck);
+    free(arg);
+
+    // Signal the semaphore to indicate a client has finished
+    sem_post(&client_sem);
+
+    pthread_exit(NULL);
+}
+
+int main() {
+    int ssck;
+    struct sockaddr_in saddr;
+    pthread_t thread_id;
+
+    // Print version information
+    printf("Server Version: %s\n", VERSION);
+
+    // Set up signal handler for SIGINT (Ctrl+C)
+    if (signal(SIGINT, signal_handler) == SIG_ERR) {
+        perror("Error setting signal handler");
+        exit(EXIT_FAILURE);
+    }
+
+    // Create a socket
+    if ((ssck = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+
+    saddr.sin_family = AF_INET;
+    saddr.sin_port = htons(SERVER_PORT);
+    saddr.sin_addr.s_addr = INADDR_ANY;
+
+    // Bind the socket to the port
+    if (bind(ssck, (struct sockaddr *)&saddr, sizeof(saddr)) < 0) {
+        perror("bind");
+        exit(EXIT_FAILURE);
+    }
+
+    // Listen for incoming connections
+    if (listen(ssck, 5) < 0) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Server is listening on port %d\n", SERVER_PORT);
+
+    // Initialize shared memory
+    int shm_id = shmget(SHM_KEY, sizeof(struct shared_data), IPC_CREAT | 0666);
+    if (shm_id == -1) {
+        perror("shmget");
+        exit(EXIT_FAILURE);
+    }
+
+    // Initialize the semaphore
+    if (sem_init(&client_sem, 0, MAX_CLIENTS) == -1) {
+        perror("sem_init");
+        exit(EXIT_FAILURE);
+    }
+
+    while (running) {
+        struct thread_arg *targ = malloc(sizeof(struct thread_arg));
+        if (!targ) {
+            perror("malloc");
+            exit(EXIT_FAILURE);
+        }
+
+        // Wait for a semaphore signal (only allow MAX_CLIENTS concurrent clients)
+        sem_wait(&client_sem);
+
+        // Accept a connection from a client
+        socklen_t caddrlen = sizeof(targ->caddr);
+        targ->csck = accept(ssck, (struct sockaddr *)&targ->caddr, &caddrlen);
+        if (targ->csck < 0) {
+            perror("accept");
+            free(targ);
+            // Signal the semaphore since this client failed to connect
+            sem_post(&client_sem);
+            continue;
+        }
+
+        // Create a thread to handle the client
+        if (pthread_create(&thread_id, NULL, handle_client, (void *)targ) != 0) {
+            perror("pthread_create");
+            close(targ->csck);
+            free(targ);
+            // Signal the semaphore since this client failed to create a thread
+            sem_post(&client_sem);
+        }
+
+        // Detach the thread
+        pthread_detach(thread_id);
+    }
+
+    // Cleanup
+    close(ssck);
+    sem_destroy(&client_sem);
+
+    return 0;
 }
 
--- bbg/build/src/tcp_client.c
+++ bbg/build/src/tcp_client.c
@@ -1,92 +1,120 @@
-/* gcc tcp_client.c -o out_tcp_client */
-/* tcp_client.c */
-
-#include <stdio.h>         /* Standard I/O library */
-#include <stdlib.h>        /* Standard library for general functions */
-#include <string.h>        /* String handling functions */
-#include <unistd.h>        /* POSIX API */
-#include <sys/types.h>     /* Data types used in system calls */
-#include <sys/stat.h>      /* File status and permission functions */
-#include <fcntl.h>         /* File control options */
-#include <errno.h>         /* Error number definitions */
-#include <arpa/inet.h>     /* Definitions for internet operations */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <arpa/inet.h>
 
 #define FIFO_PATH   "tmp/gps_pipe"   /* Path to the FIFO file */
 #define SERVER_PORT 12345            /* Server port number */
-#define SERVER_IP   "192.168.15.188" /* Server IP address */
 #define BUFFER_SIZE 1024             /* Buffer size for reading and writing data */
+
+void get_server_ip(char *ip_buffer, size_t buffer_size)
+{
+    FILE *fp;
+    char cmd[] = "hostname -I | awk '{print $1}'";
+
+    fp = popen(cmd, "r");
+    if (fp == NULL) {
+        perror("popen");
+        exit(EXIT_FAILURE);
+    }
+
+    if (fgets(ip_buffer, buffer_size, fp) == NULL) {
+        perror("fgets");
+        exit(EXIT_FAILURE);
+    }
+
+    // Remove trailing newline character
+    size_t len = strlen(ip_buffer);
+    if (len > 0 && ip_buffer[len-1] == '\n') {
+        ip_buffer[len-1] = '\0';
+    }
+
+    pclose(fp);
+}
 
 int main()
 {
-	int                 fifo_fd;              /* Descriptor for FIFO file */
-	int                 sock;                 /* Descriptor for the socket */
-	struct sockaddr_in  saddr;          /* Structure to store server address */
-	char                buffer[BUFFER_SIZE];  /* Buffer to hold data */
-	ssize_t             brd;           /* Number of bytes read */
+    int fifo_fd;              /* Descriptor for FIFO file */
+    int sock;                 /* Descriptor for the socket */
+    struct sockaddr_in saddr; /* Structure to store server address */
+    char buffer[BUFFER_SIZE]; /* Buffer to hold data */
+    ssize_t brd;              /* Number of bytes read */
+    char server_ip[INET_ADDRSTRLEN];  /* Buffer to hold the server IP address */
 
+    // Get the server IP address at runtime
+    get_server_ip(server_ip, sizeof(server_ip));
 
-	/* Open the FIFO in read-only mode */
-	fifo_fd = open(FIFO_PATH, O_RDONLY);
-	if (fifo_fd == -1)
-	{
-		perror("Error opening FIFO");
-		exit(EXIT_FAILURE);
-	}
+    /* Open the FIFO in read-only mode */
+    fifo_fd = open(FIFO_PATH, O_RDONLY);
+    if (fifo_fd == -1)
+    {
+        perror("Error opening FIFO");
+        exit(EXIT_FAILURE);
+    }
 
-	/* Create a TCP socket */
-	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-	{
-		perror("socket");
-		exit(EXIT_FAILURE);
-	}
+    /* Create a TCP socket */
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
 
-	/* Initialize the server address structure */
-	saddr.sin_family = AF_INET;                   /* Use IPv4 address family */
-	saddr.sin_port = htons(SERVER_PORT);          /* Set server port, converting to network byte order */
-	saddr.sin_addr.s_addr = inet_addr(SERVER_IP); /* Set server IP address */
+    /* Initialize the server address structure */
+    saddr.sin_family = AF_INET;                   /* Use IPv4 address family */
+    saddr.sin_port = htons(SERVER_PORT);          /* Set server port, converting to network byte order */
+    if (inet_pton(AF_INET, server_ip, &saddr.sin_addr) <= 0)
+    {
+        perror("inet_pton");
+        exit(EXIT_FAILURE);
+    }
 
-	/* Connect to the server */
-	if (connect(sock, (struct sockaddr *)&saddr, sizeof(saddr)) < 0)
-	{
-		perror("connect");
-		exit(EXIT_FAILURE);
-	}
+    /* Connect to the server */
+    if (connect(sock, (struct sockaddr *)&saddr, sizeof(saddr)) < 0)
+    {
+        perror("connect");
+        exit(EXIT_FAILURE);
+    }
 
-	/* Read from the FIFO and send data to the server */
-	while (1)
-	{
-		brd = read(fifo_fd, buffer, sizeof(buffer) - 1); /* Leave space for null-terminator */
-		if (brd > 0)
-		{
-			buffer[brd] = '\0';            /* Null-terminate the string */
-			printf("Read from FIFO: %s", buffer); /* Print the read data */
-			fflush(stdout);                       /* Flush the output buffer */
+    /* Read from the FIFO and send data to the server */
+    while (1)
+    {
+        brd = read(fifo_fd, buffer, sizeof(buffer) - 1); /* Leave space for null-terminator */
+        if (brd > 0)
+        {
+            buffer[brd] = '\0';            /* Null-terminate the string */
+            printf("Read from FIFO: %s", buffer); /* Print the read data */
+            fflush(stdout);                       /* Flush the output buffer */
 
-			/* Send data to the server */
-			if (send(sock, buffer, strlen(buffer), 0) < 0)
-			{
-				perror("send");
-				exit(EXIT_FAILURE);
-			}
+            /* Send data to the server */
+            if (send(sock, buffer, strlen(buffer), 0) < 0)
+            {
+                perror("send");
+                exit(EXIT_FAILURE);
+            }
 
-			printf("Sent to server: %s", buffer); /* Print the sent data */
+            printf("Sent to server: %s", buffer); /* Print the sent data */
 
-		}
-		else if (brd == 0)
-		{
-			break;  /* End of data in FIFO */
-		}
-		else if (brd == -1 && errno != EAGAIN)
-		{
-			perror("Error reading from FIFO");
-			exit(EXIT_FAILURE);
-		}
-	}
+        }
+        else if (brd == 0)
+        {
+            break;  /* End of data in FIFO */
+        }
+        else if (brd == -1 && errno != EAGAIN)
+        {
+            perror("Error reading from FIFO");
+            exit(EXIT_FAILURE);
+        }
+    }
 
-	/* Close the FIFO and socket */
-	close(fifo_fd);
-	close(sock);
+    /* Close the FIFO and socket */
+    close(fifo_fd);
+    close(sock);
 
-	return 0;
+    return 0;
 }
 
