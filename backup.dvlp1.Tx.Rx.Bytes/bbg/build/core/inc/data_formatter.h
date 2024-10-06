dd33c4bcfb562de356d928c8528f69b8d5e59bdb
morris <morris.elkanaev@gmail.com>
2024-10-06 23:28:22 +0300

    delete old files

--- backup.dvlp1.Tx.Rx.Bytes/bbg/build/core/inc/data_formatter.h
+++
@@ -1,7 +0,0 @@
-#ifndef DATA_FORMATTER_H
-#define DATA_FORMATTER_H
-
-void process_received_message(const char* received_message);
-
-#endif  /* DATA_FORMATTER_H */
-
--- backup.dvlp1.Tx.Rx.Bytes/must_read_before_using.md
+++
@@ -1,3 +0,0 @@
-## This Version pointed to BBG
-1. Used by inotify for send data from file
-2. Getted data saved into the temporary file
